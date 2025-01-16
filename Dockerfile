52714bfe57889fff0c1106aa29f813e29337d8ab
monu123-cloud <mukrsharma2812@gmail.com>
2025-01-16 15:11:45 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM  openjdk:17-jdk-slim
+
+ADD ./target/*.jar /app/app.jar
+
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]x1
---
+++ src/main/java/com/example/test/TestController.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping("/getName")
+   public String getResponse(){
+        return "monu Kumar Sharma";
+    }
+
+
+}
